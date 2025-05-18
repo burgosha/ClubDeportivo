f3635884b0518055c8827161721964496ad0ceca
burgosha <hernan.a.burgos@gmail.com>
2025-05-18 01:20:16 -0300

    correccion de errores

--- app/src/main/java/com/example/clubdeportivo/Clientes.kt
+++ app/src/main/java/com/example/clubdeportivo/Clientes.kt
@@ -6,11 +6,11 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class aniadir_actividad : AppCompatActivity() {
+class Clientes : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_aniadir_actividad)
+        setContentView(R.layout.activity_clientes)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
